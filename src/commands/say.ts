c88b2dcd0dabaacca7794e544aa0e421fad3036e
ThatDudeKondd <ThatDudeKondd@protonmail.com>
2026-07-31 00:04:08 +0200

    Cleaned up code.

--- src/commands/say.ts
+++ src/commands/say.ts
@@ -1,24 +1,17 @@
-import { DiscordAPIError } from "discord.js";
 import type { Command } from "./registry";
 
 const sayCommand: Command = {
   name: "say",
   summary: "Says the given message.",
   async handler(ctx) {
-    console.log("say handler", ctx.message?.id);
     const args = ctx.args;
     const message = args.join(" ");
 
     try {
       await ctx.message?.delete();
-    } catch (err: any) {
-      console.log({
-        constructor: err?.constructor?.name,
-        name: err?.name,
-        code: err?.code,
-      });
-
-      if (err?.code === 10008) {
+    } catch (err) {
+      const code = (err as { code?: unknown })?.code;
+      if (code !== 10008) {
         throw err;
       }
     }
